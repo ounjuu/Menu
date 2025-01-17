adda2006dc3101c5a9a6d0350b893f7dca0fe886
ounjuu <koj1555@naver.com>
2025-01-17 12:15:51 +0900

    Feat: nodejs

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.listen(8088, "0.0.0.0", () => {
-  console.log(`Server running at http://0.0.0.0:${8088}/`);
+app.use(express.static(__dirname + "/public"));
+
+app.listen(8080, () => {
+  console.log(`Server running at http://localhost:${8080}/`);
 });
 
 app.get("/", (요청, 응답) => {
   응답.send("반갑다");
+});
+
+app.get("/news", (요청, 응답) => {
+  응답.sendFile(__dirname + "/index.html");
+});
+app.get("/shop", (요청, 응답) => {
+  응답.send("쇼핑페이지임");
+});
+app.get("/about", (요청, 응답) => {
+  응답.sendFile(__dirname + "/aboutme.html");
 });
